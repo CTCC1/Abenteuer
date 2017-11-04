600dfd0647339b71837c25ec670c79c8a0ac7e74
Hao Mou <sora@Haos-MacBook-Pro.local>
2017-11-04 12:48:26 -0500

    Simple UI implemented.

--- Explore/ViewController.swift
+++ Explore/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationInfo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    var i = 0
+    
+    @IBAction func pressButton(_ sender: UIButton) {
+        if i == 0 {
+            locationInfo.text = "(still nothing)"
+            i += 1
+        } else {
+            locationInfo.text = "(Nothing yet)"
+        }
+        
+    }
+    
 }
 
